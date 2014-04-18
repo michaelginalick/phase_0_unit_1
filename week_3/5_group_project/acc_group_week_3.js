7a191024d1b6bdc19a597c6a6223612e92f69214
michaelginalick <michael.ginalick@gmail.com>
2014-04-18 14:41:08 -0500

    Create acc_group_week_3.js

---
+++ week_3/5_group_project/acc_group_week_3.js
@@ -0,0 +1,26 @@
+
+I would like to create a series of numbered tests with passing and failing conditions. 
+If the tests passes, I would like to see the number of the test coupled with it's corresponding number. 
+However, if the test fails, I would like to see the word 'ERROR' along with what the test is expecting for it to be true, 
+and the corresponding test number.
+ 
+For these tests, I would like to use two groups of numbers. 
+One with an even amount of numbers in the group and one with an odd amount in it's respective group.
+ 
+1) for the first test, I would like to see a test which will tell me the sum of the test in addition to what tool was used to get the answer. Plus the test number.
+ 
+2) for the second test, I would like to see the total value of all the numbers in the odd group of numbers. If the sum of the numbers equal 27, it passes. If the total does not equal 27, I would like to tell the user that the sum needs to be 27 along with the test number.
+ 
+3) for the third test, I would like to see the total value of all the numbers in the even group of numbers. If the sum of the numbers is equal to 43, it passes. If the total does not equal 43, I would like to tell the user that the sum needs to be 43 along with the test number.
+ 
+4) for the forth test, I would like to see a test which will tell me the mean of the test in addition to what tool was used to get the answer. Plus the test number.
+ 
+5) for the fifth test, I would like to see the average of all the numbers in the odd group of numbers. If the average doesn't equal '3.857142857142857', I would like to tell the user that the average of all the numbers should eqal  3.857142857142857 along with the test number.
+ 
+6) for the sixth test, I would like to see the average of all the numbers in the even group of numbers. If the average doesn't equal '5.375', I would like to tell the user that the average of all the numbers should eqal '5.375' along with the test number.
+ 
+7) for the seventh test, I would like to see a test which will tell me the median of the test in addition to what tool was used to get the answer. Plus the test number.
+ 
+8) for the eighth test, I would like to see the median value of all the numbers in the odd group of numbers. If the median doesn't equal '4', I would like to tell the user that the median of all the numbers should equal '4' along with the test number.
+ 
+9) for the ninth test, I would like to see the mediam value of all the numbers in the even group of numbers. If the median doesn't equal '5.5', I would like to tell the user that the median of all the numbers should equal '5.5' along with the test number.
